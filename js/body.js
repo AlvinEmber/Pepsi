5a40d1800fe8cf011f432949ed63274a4e3ae1db
AlvinEmber <yafamoster@163.com>
2021-10-28 09:12:25 +0800

    '字体修改'

---
+++ js/body.js
@@ -0,0 +1,13 @@
+// 全局js
+window.addEventListener("load", function () {
+    // 头部
+    // navLogo 点击返回主页
+    var navLogo = document.getElementsByClassName("navLogo")[0];
+    // navTitle 点击打开商品菜单
+    var navTitle = document.getElementsByClassName("navTitle")[0];
+    // navListIcon 点击打开下拉菜单
+    var navListIcon = document.getElementsByClassName("navListIcon")[0];
+    touch.on(navListIcon, 'tap', function () {
+        
+    })
+})
